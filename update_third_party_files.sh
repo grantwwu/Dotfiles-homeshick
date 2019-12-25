04fef1e9c2bb7781d27f227fd939f5f509464aed
Grant Wu <grant.wu@uber.com>
2019-12-25 01:36:48 -0500

    Add script for updating third party files
    One-ofs that I don't want to go through a whole submodule dance for

---
+++ update_third_party_files.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl -o home/.zsh_completion/_bazel https://raw.githubusercontent.com/bazelbuild/bazel/master/scripts/zsh_completion/_bazel
+curl -o home/.dir_colors https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark
